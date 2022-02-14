1bc2e8ea93b83f1c0cb5d71548996249a0f29ea9
MS_KANG <MS_KANG@pubk4006.local>
2022-02-14 10:10:00 +0900

    member css/ 서비스 및 파일 추가

--- src/main/java/com/theater/controller/LoginController.java
+++ src/main/java/com/theater/controller/LoginController.java
@@ -17,5 +17,5 @@ public class LoginController {
 	public void login() {
 		
 	}//login.do
-
+	
 }
---
+++ src/main/java/com/theater/service/MembersServiceimpl.java
@@ -0,0 +1,8 @@
+package com.theater.service;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MembersServiceimpl implements EventService {
+
+}
