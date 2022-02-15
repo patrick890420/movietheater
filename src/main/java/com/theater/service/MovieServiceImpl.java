f89a9025376c3a917ef1d76478626df7305d9dfc
AL <AL@LAPTOP-01IB83C5.local>
2022-02-15 09:46:53 +0900

    Movie추가

--- src/main/java/com/theater/service/MovieServiceImpl.java
+++ src/main/java/com/theater/service/MovieServiceImpl.java
@@ -1,7 +1,10 @@
 package com.theater.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.theater.domain.Criteria;
 import com.theater.domain.MovieVO;
 import com.theater.mapper.MovieMapper;
 
@@ -16,6 +19,31 @@ public class MovieServiceImpl implements MovieService {
   @Override
     public void MovieInsertPro(MovieVO mvo) {
     mapper.MovieInsertPro(mvo);
+    
+  }
+  
+  @Override
+  public List<MovieVO> MovieList(Criteria cri) {
+    
+    return mapper.MovieListPaging(cri);
+  }
+  
+  @Override
+  public MovieVO read(int m_cd) {
+    mapper.readCount(m_cd);
+    return mapper.read(m_cd);
+  }
+  @Override
+  public MovieVO nextPage(int m_cd) {
+    return mapper.nextPage(m_cd);
+  }
+  @Override
+  public MovieVO prevPage(int m_cd) {
+    return mapper.prevPage(m_cd);
+  }
+  @Override
+  public int getTotalCount(Criteria cri) {
+    return mapper.getTotalCount(cri);
   }
 
 }//Impl
