df954289e3f84eae1f4318f39ac76f4f2572bc53
huns <zrkddudgns@gmail.com>
2022-02-15 09:42:27 +0900

    수정

--- src/main/java/com/theater/mapper/TheaterMapper.java
+++ src/main/java/com/theater/mapper/TheaterMapper.java
@@ -6,6 +6,7 @@ import com.theater.domain.TheatersVO;
 
 public interface TheaterMapper {
   
-  public List<TheatersVO> theaterList();//극장리스트
-
+  public List<TheatersVO> theaterList(int t_area);//극장리스트
+  public List<TheatersVO> theaterinfo(String t_name);//극장정보
+  public List<TheatersVO> theatercity(int t_area);//도시정보
 }
