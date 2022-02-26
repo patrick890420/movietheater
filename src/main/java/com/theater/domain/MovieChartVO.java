e6a023e0e48792eaf9e3ef92aa615483f55716b9
AL <AL@LAPTOP-01IB83C5>
2022-02-26 23:58:35 +0900

    view페이지 차트 밑작업

---
+++ src/main/java/com/theater/domain/MovieChartVO.java
@@ -0,0 +1,18 @@
+package com.theater.domain;
+
+import lombok.Data;
+
+@Data
+public class MovieChartVO {
+
+  
+  private int m_cd;
+  private int t_cd;
+  private int t_m_cd;
+  private String gender;
+  private String birth;
+  private String id;
+  private String userid;
+  
+  
+}
